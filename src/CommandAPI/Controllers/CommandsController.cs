6feab00e1407168e359d11f59cfdea65b22b0838
Siim Bobkov <siim@warren.io>
2020-07-03 15:54:00 +0300

    replace mock return with database

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,18 +1,19 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CommandsController: ControllerBase
+    public class CommandsController : ControllerBase
     {
+        private readonly CommandDBContext _context;
 
+        public CommandsController(CommandDBContext context) => _context = context;
+
+        //GET api/commands
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "this", "is", "hard", "coded" };
-        }
-
+        public ActionResult<IEnumerable<Command>> GetCommandItems() => _context.CommandItems;
     }
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -7,16 +7,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using CommandAPI.Models;
 
 namespace CommandAPI
 {
     public class Startup
     {
+
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // Secti
+
+            services.AddDbContext<CommandDBContext>(opt=>
+            opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
+
+            // Section 1
             services.AddControllers();
         }
 
