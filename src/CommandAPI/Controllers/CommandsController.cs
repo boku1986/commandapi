b55044d03019f19a04a4d9a269a564e26168f1fc
Siim Bobkov <siim@warren.io>
2020-07-05 23:06:31 +0300

    start with xUnit tests

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using CommandAPI.Models;
+using System;
 
 namespace CommandAPI.Controllers
 {
@@ -15,5 +16,18 @@ namespace CommandAPI.Controllers
         //GET api/commands
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetCommandItems() => _context.CommandItems;
+
+
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandItem(int id)
+        {
+            var commandItem = _context.CommandItems.Find(id);
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
+            return commandItem;
+        }
     }
 }
